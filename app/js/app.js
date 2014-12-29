ecfbe73c78f0a62c07c398330df6ace0a8370f33
Ale Vilar <alevilar@gmail.com>
2014-12-29 00:54:07 -0300

    funcionando para paxapos

--- app/js/app.js
+++ app/js/app.js
@@ -1,59 +1,34 @@
 //var pppp = require("printer");
 console.debug("iniciando Appp");
 
+
+
+function ValidURL(str) {
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+
+  var pattern = new RegExp(expression); // fragment locater
+  if(!pattern.test(str)) {
+    alert("Please enter a valid URL.");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+
+
+
+
 App = Ember.Application.create({
 
 	start: function () {	
 
 		console.debug("Iniciando Ember App");
+
+
+
 		
-		App.printerJobsController = Ember.ArrayController.create({
-			content: [],
-
-			init: function () {
-				this.jobs = new BBApp.Jobs;
-				var self = this;
-
-				// al agregar un job, debo agregarlo tambien a este controller
-				this.jobs.on('add', function(job) {
-					console.info("App.printerJobsController:: Hay un nuevo trabajo por imprimir %o", self);
-					self.unshiftObject( job.toJSON() );
-					if ( self.length > 5 ) {
-						self.popObject();
-					}
-				});
-
-
-				// al eliminar eun job, tambien devo quitar del controller
-				this.jobs.on('remove', function(job) {
-					//self.popObject( );
-				});
-
-
-
-
-				// verificar si estoy usando un browser "normal"
-				if ( isNodeJs() ) {
-					// si aun no configure el Host, que me muestre la pantalla al comienzo
-					// es un proceso de configuracion inicial
-					var gui = require('nw.gui');
-					if ( !localStorage.getItem('urlHost') ) {
-						gui.Window.get().show();	
-					}	
-				}
-
-
-				// chequeo que no se corte nunca. y si se corta, lo vuelvo a arrancar
-				var interval = setInterval( function () {
-					if ( !App.printerJobsController.jobs.fetching ) {
-						App.printerJobsController.jobs.startFetch();
-					}
-				}, 13000 );
-
-				
-			}
-
-		});
+		
 	}
 });
 
@@ -70,30 +45,100 @@ App.Router.map( function() {
 
 App.ApplicationController = Ember.Controller.extend({
   online: "red",
-  //urlHost: "",
+  //urlHost: "http://www.paxapos.com/site-name",
+  hostName: "http://www.fray.alevilar.com",
   siteName: "",
   reloadInterval: 3000,
   minInterval: 800,
+  logs: "",
 
   	init: function () {
   		this._super();
+
+  		this.__initLocalStorage();
+  		
+  		this.__setLoginIframe();
+
+  		this.__InitJobs();
+
+  		// finalmente chequeo que haya conexion
+  		this.checkOnline();
+  	},
+
+  	__setLoginIframe: function () {
+  		var hostName = this.getFullUrlSiteName() + "printers/printer_jobs/login";
+console.debug(hostName);
+  		$( function(){
+  			$("#login-iframe").html("<iframe src='" + hostName + "' width='100%' heigth='100px'></iframe>")
+  		});
+  		
+  		return true;
+  	},
+
+  	__initLocalStorage: function () {
   		this.set('siteName', localStorage.getItem("siteName") );
   		this.set('reloadInterval', localStorage.getItem("reloadInterval") );
 
+  		var hostName = localStorage.getItem("hostName");
+  		if ( hostName ) {
+  			this.set('hostName', localStorage.getItem("hostName") );
+  		} else {
+  			localStorage.setItem("hostName", this.get("hostName"));
+  		}
+  	},
 
+  	__InitJobs: function () {
+  		this.jobs = new BBApp.Jobs;
   		var self = this;
+
+
+		// al agregar un job, debo agregarlo tambien a este controller
+		this.jobs.on('add', function(job) {
+			console.info("App.printerJobsController:: Hay un nuevo trabajo por imprimir %o, %o", self, job);
+			self.logs = "Nuevo trabajo\n" + self.logs;
+		});
+
+
+		// al eliminar eun job, tambien devo quitar del controller
+		this.jobs.on('remove', function(job) {
+			console.info("Job remove:: se elimino un trabajo %o", job);
+			self.logs = "Se borro un trabajo\n" + self.logs;
+			//self.popObject( );
+		});
+
+
+		// verificar si estoy usando un browser "normal"
+		if ( isNodeJs() ) {
+			// si aun no configure el Host, que me muestre la pantalla al comienzo
+			// es un proceso de configuracion inicial
+			var gui = require('nw.gui');
+			if ( !localStorage.getItem('urlHost') ) {
+				alert("Falta configurar el HOST");
+				gui.Window.get().show();	
+			}	
+		}
+
+
+		// chequeo que no se corte nunca. y si se corta, lo vuelvo a arrancar
+		var self = this;
+		var interval = setInterval( function () {
+			if ( !self.jobs.fetching ) {
+				self.jobs.startFetch();
+			}
+		}, 13000 );
+		
+
+
   		// sincronizo e intercepto errores para GUI
-  		App.printerJobsController.jobs.on('sync', function () {
+  		this.jobs.on('sync', function () {
   			self.set("online", 'green');
   		});
-  		App.printerJobsController.jobs.on('error', function () {  	
-  			App.printerJobsController.jobs.stopFetch();
+  		this.jobs.on('error', function () {  	
+  			self.jobs.stopFetch();
   			self.set("online", 'yellow');
   			self.checkOnline();
   		});
 
-  		// finalmente chequeo que haya conexion
-  		self.checkOnline();
   	},
 
   	actions: {
@@ -133,6 +178,26 @@ App.ApplicationController = Ember.Controller.extend({
   		return '';
   	}.property('online'),
 
+  	
+
+	retryFetch: function () {
+		var online = this.get("online");
+		if ( online != "red" ) {
+			this.jobs.startFetch();
+		}
+	}.observes('online'),
+
+
+	/**
+	*
+	* @return String Ej:"http://localhost/"+siteName;
+	*
+	**/
+	getFullUrlSiteName: function () {
+		return this.get("hostName") + "/" + this.get('siteName') + "/";
+	},
+
+
   	updateReloadInterval: function(){	
 
   			var interval = parseInt( this.get("reloadInterval") );
@@ -146,35 +211,32 @@ App.ApplicationController = Ember.Controller.extend({
   				$("#g-reload-interval").removeClass('has-success');
   				$('.error', "#g-reload-interval").show();
   			}
-	  		
-	  		
-	  }.observes('reloadInterval'),
+  	}.observes('reloadInterval'),
 
 
-	retryFetch: function () {
-		var online = this.get("online");
-		if ( online != "red" ) {
-			App.printerJobsController.jobs.startFetch();
-		}
-	}.observes('online'),
+	updateHostName: function () {
+	  		localStorage.setItem("hostName", this.get("hostName"));
+	  		this.updateUrlHost();
+	  		this.__setLoginIframe();
+  	}.observes('hostName'),
 
-	completeUrlHostWithSiteName: function ( siteName ) {
+  	updateSiteName: function(){	
+  			this.updateUrlHost();
+  			this.__setLoginIframe();
+	}.observes('siteName'),
 
-		return "http://"+siteName+".paxapos.com";
-		//return "http://localhost/"+siteName;
-	},
 
-  	updateUrlHost: function(){	
+	updateUrlHost: function(){	
   			var siteName = this.get('siteName');
-  			var url = this.completeUrlHostWithSiteName( siteName );
-  			var urlHost = require("url").parse( url );
+  			var url = this.getFullUrlSiteName();
+  			var urlHost = localStorage.getItem("urlHost");
 
-  			if ( urlHost.host && urlHost.protocol ) {
+  			if ( ValidURL( urlHost ) ) {
   				// esta ok
   				localStorage.setItem("siteName", siteName );
-	  			localStorage.setItem("urlHost", urlHost.href );
-	  			$("#g-url-host").addClass("has-success");
+	  			localStorage.setItem("urlHost", urlHost );
 	  			$("#g-url-host").removeClass("has-error");
+	  			$("#g-url-host").addClass("has-success");
 	  			$('.error', "#g-url-host").hide();
   			} else {
   				// hay error
